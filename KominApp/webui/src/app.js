efc7cbe41c85278b614c8cf5458a40ed6800cd3a
madeit <mmati5085@gmail.com>
2023-06-01 20:05:09 +0200

    example table

---
+++ KominApp/webui/src/app.js
@@ -0,0 +1,10 @@
+import './app.css';
+import Table from './components/table';
+const App = () => {
+    return (
+        <div className="app">
+            <Table />
+        </div>
+    )
+}
+export default App;
